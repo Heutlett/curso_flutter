70b2552ea011d282b8d379409a53ac71c7cda2ea
heutlett <44324449+Heutlett@users.noreply.github.com>
2023-01-31 14:54:58 -0600

    add: organizacion de proyecto video 34

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,4 @@
-import 'package:curso_app/pages/second_page.dart';
 import 'package:flutter/material.dart';
-import 'pages/my_home_page.dart';
+import 'package:curso_app/src/app.dart';
 
 void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      //home: MyHomePage(),
-      initialRoute: "/",
-      routes: {
-        "/": (BuildContext context) => MyHomePage(),
-        "/second": (BuildContext context) => SecondPage()
-      },
-    );
-  }
-}
---
+++ lib/src/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/my_home_page.dart';
+import 'screens/second_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      //home: MyHomePage(),
+      initialRoute: "/",
+      routes: {
+        "/": (BuildContext context) => MyHomePage(),
+        "/second": (BuildContext context) => SecondPage()
+      },
+    );
+  }
+}
